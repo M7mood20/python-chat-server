5fb9f63fa90a16ed874cff6f4337b5f12b3b5066
M7mood20 <me405609@gmail.com>
2026-05-16 14:41:26 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('0.0.0.0', 65432))
+server.listen()
+
+clients = []
+
+def broadcast(message, sender_conn):
+    for client in clients:
+        if client != sender_conn:
+            try:
+              
+                client.sendall(message)
+            except:
+                clients.remove(client)
+
+def handle_client(conn, addr):
+    print(f"Connected: {addr}")
+    while True:
+        try:
+            data = conn.recv(1024 * 1024) 
+            if not data: break
+            broadcast(data, conn)
+        except: break
+    conn.close()
+
+print("Server is ready...")
+while True:
+    conn, addr = server.accept()
+    clients.append(conn)
+    threading.Thread(target=handle_client, args=(conn, addr), daemon=True).start()
